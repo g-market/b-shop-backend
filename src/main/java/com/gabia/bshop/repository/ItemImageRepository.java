d599877e78617404c5a24494599456be5602a6fe
Summer <cmlee@gabia.com>
2023-02-02 11:21:31 +0900

    feat: Entity Repository 추가

---
+++ src/main/java/com/gabia/bshop/repository/ItemImageRepository.java
@@ -0,0 +1,8 @@
+package com.gabia.bshop.repository;
+
+import com.gabia.bshop.entity.ItemImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImageRepository extends JpaRepository<ItemImage, Long> {
+
+}
