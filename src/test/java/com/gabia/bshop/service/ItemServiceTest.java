65343a8bb8aec28d3305e4c9c6a6db578061a448
백승주 <sjbaek@gabia.com>
2023-02-03 09:45:54 +0900

    feat : 상품 CRUD 기능 구현(2) - 테스트 코드 init

---
+++ src/test/java/com/gabia/bshop/service/ItemServiceTest.java
@@ -0,0 +1,25 @@
+package com.gabia.bshop.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceTest {
+
+    @Test
+    void 상품_조회() {}
+
+    @Test
+    void 상품_목록_조회() {}
+
+    @Test
+    void 상품_정보_수정() {}
+
+    @Test
+    void 상품_제거() {}
+    @Test
+    void 상품_재고_변경() {}
+
+}
