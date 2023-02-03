02eda5097c4d162ad2fa05bf46ec189b35fc7dc0
백승주 <sjbaek@gabia.com>
2023-02-03 13:47:19 +0900

    feat : 상품 CRUD 테스트(2) - 상품 정보 수정

--- src/test/java/com/gabia/bshop/service/ItemServiceTest.java
+++ src/test/java/com/gabia/bshop/service/ItemServiceTest.java
@@ -4,9 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
+import com.gabia.bshop.dto.ItemDto;
 import com.gabia.bshop.entity.Category;
 import com.gabia.bshop.entity.Item;
 import com.gabia.bshop.entity.enumtype.ItemStatus;
+import com.gabia.bshop.mapper.ItemMapper;
 import com.gabia.bshop.repository.CategoryRepository;
 import com.gabia.bshop.repository.ItemRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -19,6 +21,7 @@ import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -32,11 +35,15 @@ class ItemServiceTest {
     @InjectMocks
     private ItemService itemService;
 
+    Item item1;
+    Item item2;
+
+    List<Item> itemList;
     @BeforeEach
     void beforeEach() {
         //given
         Category category = Category.builder().id(1L).name("name").build();
-        Item item1 = Item.builder()
+        item1 = Item.builder()
                 .id(1L)
                 .category(category)
                 .name("item")
@@ -46,7 +53,7 @@ class ItemServiceTest {
                 .deleted(false)
                 .openAt(LocalDateTime.now()).build();
 
-        Item item2 = Item.builder()
+        item2 = Item.builder()
                 .id(2L)
                 .category(category)
                 .name("item2")
@@ -57,7 +64,7 @@ class ItemServiceTest {
                 .openAt(LocalDateTime.now()).build();
 
 
-        when(categoryRepository.findById(anyLong())).thenReturn(Optional.ofNullable(category));
+        when(categoryRepository.findById(1L)).thenReturn(Optional.ofNullable(category));
 
         //존재하는 상품
         when(itemRepository.findById(1L)).thenReturn(Optional.ofNullable(item1));
@@ -65,6 +72,7 @@ class ItemServiceTest {
 
         //존재하지 않는 상품
         when(itemRepository.findById(3L)).thenThrow(EntityNotFoundException.class);
+        when(categoryRepository.findById(2L)).thenThrow(EntityNotFoundException.class);
 
     }
 
@@ -82,8 +90,81 @@ class ItemServiceTest {
 
     @Test
     void 상품_정보_수정() {
+        //given
+        Category category = Category.builder().id(1L).name("name").build();
+        Item item = Item.builder()
+                .id(2L)
+                .category(category)
+                .name("item2")
+                .itemStatus(ItemStatus.PUBLIC)
+                .basePrice(20000)
+                .description("changed")
+                .deleted(false)
+                .openAt(LocalDateTime.now()).build();
 
+        ItemDto itemDto = ItemMapper.INSTANCE.itemToDto(item);
+
+        //when
+        when(itemRepository.save(item)).thenReturn(item);
+
+        //then
+        ItemDto changeedItem = itemService.updateItem(itemDto);
+
+        assertAll(
+                () -> assertEquals(20000, changeedItem.basePrice()),
+                () -> assertEquals("changed", changeedItem.description())
+        );
+    }    @Test
+    void 상품_수정_실패_상품_없음() {
+        //given
+        Category category = Category.builder().id(1L).name("name").build(); //존재하지 않는 카테고리
+
+        Item item = Item.builder()
+                .id(3L)
+                .category(category)
+                .name("item3")
+                .itemStatus(ItemStatus.PUBLIC)
+                .basePrice(20000)
+                .description("not exist")
+                .deleted(false)
+                .openAt(LocalDateTime.now()).build();
+
+        ItemDto itemDto = ItemMapper.INSTANCE.itemToDto(item);
+
+        //when
+        when(itemRepository.save(item)).thenReturn(item);
+
+        //then
+        Assertions.assertThrows(EntityNotFoundException.class, () -> {
+            itemService.updateItem(itemDto);
+        });
     }
+    @Test
+    void 상품_수정_실패_카테고리_없음() {
+        //given
+        Category category = Category.builder().id(2L).name("name").build(); //존재하지 않는 카테고리
+        
+        Item item = Item.builder()
+                .id(2L)
+                .category(category)
+                .name("item2")
+                .itemStatus(ItemStatus.PUBLIC)
+                .basePrice(20000)
+                .description("changed")
+                .deleted(false)
+                .openAt(LocalDateTime.now()).build();
+
+        ItemDto itemDto = ItemMapper.INSTANCE.itemToDto(item);
+
+        //when
+        when(itemRepository.save(item)).thenReturn(item);
+
+        //then
+        Assertions.assertThrows(EntityNotFoundException.class, () -> {
+            itemService.updateItem(itemDto);
+        });
+    }
+    
 
     @Test
     void 상품_제거_성공() {
@@ -97,7 +178,7 @@ class ItemServiceTest {
             itemService.deleteItem(3L);
         });
     }
-
-    @Test
-    void 상품_재고_변경() {}
+//
+//    @Test
+//    void 상품_재고_변경() {}
 }
