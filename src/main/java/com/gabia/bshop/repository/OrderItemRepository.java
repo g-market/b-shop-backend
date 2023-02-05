028cfbf0175f6997598dd6e1f34571f7d12a5760
이창민 <cmlee@gabia.com>
2023-02-05 16:03:12 +0900

    feat: 주문목록 조회 API 구현, 데이터 초기화 클래스 생성 (mentee/mentee_2023.01/team/g-market/gabia_b_shop_backend!9)
    * style: 코드 가독성 향상을 위한 개행 추가
    
    * feat: custom exception 으로 변경
    
    - IllegaStateException -> EntityNotFoundException
    
    * feat: 쿼리 메서드 @Param 제거
    
    * feat: 메서드 파라미터, 필드 final 키워드 추가
    
    * feat: 존재하지 않는 회원ID로 주문목록_조회를 요청하면 오류가 발생한다.
    
    - Mockito 단위테스트
    
    * feat: 주문 후 삭제된 상품도 주문목록에서 조회되어야한다
    
    * feat: description 속성 text로 변경
    
    * rename: 통합테스트 폴더로 이동
    
    * test: 존재하지 않는 회원이 주문목록 조회를 요청하면 오류가 발생해야 한다.
    
    - OrderService 통합테스트 작성
    
    * test: 주문을 한 회원이 주문목록 조회를 수행하면 주문내역들이 조회되어야 한다.
    
    - OrderService 통합테스트 작성
    
    * style: 불필요 콘솔출력 제거
    
    * rename: OrderInfoMapper로 이름 변경
    
    * style: 불필요 공백 제거
    
    * feat: 주문내역 조회 컨트롤러 api 매핑
    
    * feat: 주문내역 조회 서비스로직 작성
    
    * feat: 주문내역 record DTO 객체 생성
    
    * feat: ItemImage 조회 쿼리메서드 추가
    
    - 셀프조인 느낌으로 구현
    - 썸네일 이미지 하나만 가져오도록 하는 의도
    - 페치조인으로 연관된 Item도 같이 조회
    
    * feat: OrderItem 조회 쿼리메서드 추가
    
    * fix: Pageable import 오류 수정
    
    * feat: OrderInfoPageResponse 매퍼 디폴트 메서드 추가
    
    * chore: test yml osiv 비활성화
    
    * feat: 로컬 데이터 초기화 역할 클래스 생성 및 구현
    
    * feat: Entity Repository 추가
    
    * style: 마지막 줄 공백 추가
    
    * feat: Orders에 빠져있던 BaseEntity 상속 추가

---
+++ src/main/java/com/gabia/bshop/repository/OrderItemRepository.java
@@ -0,0 +1,15 @@
+package com.gabia.bshop.repository;
+
+import com.gabia.bshop.entity.OrderItem;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+    @Query("select oi from OrderItem oi "
+            + "where oi.item.id "
+            + "in (:orderIds)")
+    List<OrderItem> findByOrderIds(List<Long> orderIds);
+}
