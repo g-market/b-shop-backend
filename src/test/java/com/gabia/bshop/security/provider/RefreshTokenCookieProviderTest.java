88fe7faee41b9c145a109d615c228168f3887324
정종민 <jmchung@gabia.com>
2023-02-06 10:28:42 +0900

    test: 쿠키를 생성하고 만료하는 테스트 코드 작성

---
+++ src/test/java/com/gabia/bshop/security/provider/RefreshTokenCookieProviderTest.java
@@ -0,0 +1,36 @@
+package com.gabia.bshop.security.provider;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseCookie;
+
+class RefreshTokenCookieProviderTest {
+
+    private final RefreshTokenCookieProvider provider =
+            new RefreshTokenCookieProvider(1209600000L);
+
+    @Test
+    void 리프레시_토큰으로_쿠키를_생성한다() {
+        // given
+        final String refreshTokenValue = "refreshTokenValue";
+        // when
+        ResponseCookie responseCookie = provider.createCookie(refreshTokenValue);
+        // then
+        assertAll(
+                () -> assertThat(responseCookie.getValue()).isEqualTo(refreshTokenValue),
+                () -> assertThat(responseCookie.getMaxAge()).isEqualTo(
+                        Duration.ofMillis(1209600000L))
+        );
+    }
+
+    @Test
+    void 생성된_쿠키를_만료시킨_뒤_반환한다() {
+        // when
+        ResponseCookie responseCookie = provider.createLogoutCookie();
+        // then
+        assertThat(responseCookie.getMaxAge()).isZero();
+    }
+}
