b69e076ca1f3104103897cfdd57549408b7fe1ff
정종민 <jmchung@gabia.com>
2023-02-06 11:15:41 +0900

    feat: OauthClientConfig 추가

---
+++ src/main/java/com/gabia/bshop/config/LocalOAuthClientConfig.java
@@ -0,0 +1,44 @@
+package com.gabia.bshop.config;
+
+import com.gabia.bshop.security.client.HiworksOauthClient;
+import com.gabia.bshop.security.client.LocalHiworksOauthClient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile({"local", "test"})
+public class LocalOAuthClientConfig {
+
+    @Bean
+    @Qualifier("normal")
+    public HiworksOauthClient localHiworksOauthClient(
+            @Value("${hiworks.client.id}") final String clientId,
+            @Value("${hiworks.client.secret}") final String secret,
+            @Value("${hiworks.url.accessToken}") final String accessTokenUrl,
+            @Value("${hiworks.url.user}") final String profileUrl) {
+        return LocalHiworksOauthClient.builder()
+                .clientId(clientId)
+                .secret(secret)
+                .accessTokenUrl(accessTokenUrl)
+                .profileUrl(profileUrl)
+                .build();
+    }
+
+    @Bean
+    @Qualifier("admin")
+    public HiworksOauthClient adminHiworksOauthClient(
+            @Value("${hiworks.client.admin-id}") final String clientId,
+            @Value("${hiworks.client.admin-secret}") final String secret,
+            @Value("${hiworks.url.accessToken}") final String accessTokenUrl,
+            @Value("${hiworks.url.user}") final String profileUrl) {
+        return LocalHiworksOauthClient.builder()
+                .clientId(clientId)
+                .secret(secret)
+                .accessTokenUrl(accessTokenUrl)
+                .profileUrl(profileUrl)
+                .build();
+    }
+}
