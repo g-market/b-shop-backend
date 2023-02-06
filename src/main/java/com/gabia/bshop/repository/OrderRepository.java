fe4d32cfb0bfed1bdd1f113429dcf648004c68f3
Summer <cmlee@gabia.com>
2023-02-06 13:17:28 +0900

    feat: admin 주문내역 조회 쿼리 메서드 작성
    - 현재는 검색조건이 날짜밖에 없음

--- src/main/java/com/gabia/bshop/repository/OrderRepository.java
+++ src/main/java/com/gabia/bshop/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.gabia.bshop.repository;
 
 import com.gabia.bshop.entity.Orders;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,7 @@ public interface OrderRepository extends JpaRepository<Orders, Long> {
     // TODO: created_at 인덱스 생성
     @Query("select o from Orders o where o.member.id =:memberId order by o.createdAt")
     List<Orders> findByMemberIdPagination(Long memberId, Pageable pageable);
+
+    @Query("select o from Orders o where o.createdAt >= :startAt and o.createdAt <= :endAt  order by o.createdAt")
+    List<Orders> findAllByPeriodPagination(LocalDateTime startAt, LocalDateTime endAt, Pageable pageable);
 }
