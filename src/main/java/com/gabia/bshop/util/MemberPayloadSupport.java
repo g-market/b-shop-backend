b2c26c3112a9e53362bf34740795c97a032839e5
정종민 <jmchung@gabia.com>
2023-02-06 12:34:37 +0900

    feat: MemberPayloadSupport Util 의미에 맞게 private 생성자 추가

--- src/main/java/com/gabia/bshop/util/MemberPayloadSupport.java
+++ src/main/java/com/gabia/bshop/util/MemberPayloadSupport.java
@@ -2,7 +2,10 @@ package com.gabia.bshop.util;
 
 import com.gabia.bshop.security.MemberPayload;
 import jakarta.annotation.Nullable;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MemberPayloadSupport {
 
     public static Long getLoggedInMemberId(@Nullable final MemberPayload loggedInMemberPayload) {
