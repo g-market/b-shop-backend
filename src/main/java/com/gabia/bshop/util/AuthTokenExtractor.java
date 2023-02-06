c65957e9ba6891fef64250569c57f4f9f83d922b
정종민 <jmchung@gabia.com>
2023-02-06 11:43:28 +0900

    feat: Auth Token Extractor 추가

---
+++ src/main/java/com/gabia/bshop/util/AuthTokenExtractor.java
@@ -0,0 +1,19 @@
+package com.gabia.bshop.util;
+
+import com.gabia.bshop.exception.UnAuthorizedException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthTokenExtractor {
+
+    public String extractToken(final String authorizationHeader, final String tokenType) {
+        if (authorizationHeader == null) {
+            throw new UnAuthorizedException("토큰이 존재하지 않습니다.");
+        }
+        final String[] splitHeaders = authorizationHeader.split(" ");
+        if (splitHeaders.length != 2 || !splitHeaders[0].equalsIgnoreCase(tokenType)) {
+            throw new UnAuthorizedException("토큰이 잘못된 형식입니다.");
+        }
+        return splitHeaders[1];
+    }
+}
