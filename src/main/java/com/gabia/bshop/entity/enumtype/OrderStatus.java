25b4cdb862710163281137c9f116c3314adf5ac3
Jenna <gyjeon@gabia.com>
2023-02-07 15:33:28 +0900

    feat : OrderStatus 수정

--- src/main/java/com/gabia/bshop/entity/enumtype/OrderStatus.java
+++ src/main/java/com/gabia/bshop/entity/enumtype/OrderStatus.java
@@ -1,7 +1,7 @@
 package com.gabia.bshop.entity.enumtype;
 
 public enum OrderStatus {
-	PENDING, // 처리 대기중
-	ACCEPTED, // 접수됨
-	COMPLETED, // 완료됨
+    ACCEPTED, // 접수됨
+    COMPLETED, // 완료됨
+    CANCELLED  // 취소됨
 }
