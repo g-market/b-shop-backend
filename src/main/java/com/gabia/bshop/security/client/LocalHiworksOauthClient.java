77acdb70740b60174c424a18422754d7c65187fe
정종민 <jmchung@gabia.com>
2023-02-08 13:25:42 +0900

    refactor: 매직 넘버 수정

--- src/main/java/com/gabia/bshop/security/client/LocalHiworksOauthClient.java
+++ src/main/java/com/gabia/bshop/security/client/LocalHiworksOauthClient.java
@@ -12,12 +12,17 @@ public record LocalHiworksOauthClient(
 	String profileUrl
 ) implements HiworksOauthClient {
 
+	private static final int NORMAL_BEGIN_INDEX = 6;
+	private static final int NORMAL_END_INDEX = 6;
+	private static final int ADMIN_BEGIN_INDEX = 6;
+	private static final int ADMIN_END_INDEX = 6;
+
 	@Override
 	public String getAccessToken(final String authCode) {
 		if (authCode.startsWith("normal")) {
-			return "normal" + authCode.substring(6, 8);
+			return "normal" + authCode.substring(NORMAL_BEGIN_INDEX, NORMAL_END_INDEX);
 		}
-		return "admin" + authCode.substring(5, 7);
+		return "admin" + authCode.substring(ADMIN_BEGIN_INDEX, ADMIN_END_INDEX);
 	}
 
 	@Override
