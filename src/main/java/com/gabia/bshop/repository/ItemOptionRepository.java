4cd83fa295e79e164c4cc744d211fcb8ab68e0f9
Jenna <gyjeon@gabia.com>
2023-02-13 15:16:01 +0900

    feat: merge 변경점(Options->ItemOption) 쿼리 반영 commit

--- src/main/java/com/gabia/bshop/repository/ItemOptionRepository.java
+++ src/main/java/com/gabia/bshop/repository/ItemOptionRepository.java
@@ -10,10 +10,10 @@ import com.gabia.bshop.entity.ItemOption;
 public interface ItemOptionRepository extends JpaRepository<ItemOption, Long> {
 
 	@Query("""
-		select o from Options o
-		join fetch o.item
-		where o.item.id = :itemId
-		and o.id = :optionId
+		select io from ItemOption io
+		join fetch io.item
+		where io.item.id = :itemId
+		and io.id = :optionId
 		""")
 	Optional<ItemOption> findWithOptionAndItemById(Long itemId, Long optionId);
 }
