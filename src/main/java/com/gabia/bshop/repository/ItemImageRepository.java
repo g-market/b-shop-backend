9f3e4891052fc39c17bdc0a11e8426dcf49295da
정종민 <jmchung@gabia.com>
2023-02-14 18:01:23 +0900

    style: multi String 으로 변경

--- src/main/java/com/gabia/bshop/repository/ItemImageRepository.java
+++ src/main/java/com/gabia/bshop/repository/ItemImageRepository.java
@@ -9,22 +9,28 @@ import com.gabia.bshop.entity.ItemImage;
 
 public interface ItemImageRepository extends JpaRepository<ItemImage, Long> {
 
-	@Query("select ii "
-		+ "from ItemImage ii "
-		+ "join fetch ii.item "
-		+ "where ii.id in (select min(t.id) "
-		+ "from ItemImage t "
-		+ "where t.item.id in (:itemIds) "
-		+ "group by t.item.id "
-		+ "order by t.id)")
+	@Query("""
+		select ii
+		from ItemImage ii
+		join fetch ii.item
+		where ii.id
+		in (select min(t.id)
+			from ItemImage t
+			where t.item.id in (:itemIds)
+			group by t.item.id
+			order by t.id)
+			""")
 	List<ItemImage> findWithItemByItemIds(List<Long> itemIds);
 
-    @Query("select ii.url "
-            + "from ItemImage ii "
-            + "where ii.id in (select min(t.id) "
-            + "from ItemImage t "
-            + "where t.item.id in (:itemIds) "
-            + "group by t.item.id "
-            + "order by t.id)")
-    List<String> findUrlByItemIds(List<Long> itemIds);
+	@Query("""
+		select ii.url
+		from ItemImage ii
+		where ii.id
+		in (select min(t.id)
+			from ItemImage t
+			where t.item.id in (:itemIds)
+			group by t.item.id
+			order by t.id)
+			""")
+	List<String> findUrlByItemIds(List<Long> itemIds);
 }
--- src/main/java/com/gabia/bshop/repository/OrderItemRepository.java
+++ src/main/java/com/gabia/bshop/repository/OrderItemRepository.java
@@ -9,9 +9,20 @@ import com.gabia.bshop.entity.OrderItem;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
-    @Query("select oi from OrderItem oi where oi.item.id in (:orderIdList)")
-    List<OrderItem> findByOrderIds(List<Long> orderIdList);
+	@Query("""
+		select oi
+		from OrderItem oi
+		where oi.item.id in (:orderIdList)
+		""")
+	List<OrderItem> findByOrderIds(List<Long> orderIdList);
 
-    @Query("select oi from OrderItem oi join fetch oi.item join fetch oi.order where oi.order.id =:orderId order by oi.item.id")
-    List<OrderItem> findWithOrdersAndItemByOrderId(Long orderId);
+	@Query("""
+		select oi
+		from OrderItem oi
+		join fetch oi.item
+		join fetch oi.order
+		where oi.order.id =:orderId
+		order by oi.item.id
+		""")
+	List<OrderItem> findWithOrdersAndItemByOrderId(Long orderId);
 }
