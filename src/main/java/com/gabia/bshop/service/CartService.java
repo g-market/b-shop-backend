a3ca770aec88c732e484d1f512323c431b0e0fd0
정종민 <jmchung@gabia.com>
2023-02-22 10:10:49 +0900

    feat: CartService 조회 쿼리 변경
    - querydsl에서 작성한 in 절 성능 향상 쿼리 사용

--- src/main/java/com/gabia/bshop/service/CartService.java
+++ src/main/java/com/gabia/bshop/service/CartService.java
@@ -2,10 +2,7 @@ package com.gabia.bshop.service;
 
 import static com.gabia.bshop.exception.ErrorCode.*;
 
-import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -37,27 +34,16 @@ public class CartService {
 	private void checkItemAndItemOption(final CartDto cartDto) {
 		if (!itemOptionRepository.existsByItem_IdAndIdAndStockQuantityIsGreaterThanEqual(cartDto.itemId(),
 			cartDto.itemOptionId(), cartDto.orderCount())) {
-			throw new NotFoundException(ITEM_ITEMOPTION_NOT_FOUND_EXCEPTION);
+			throw new NotFoundException(ITEM_OPTION_NOT_FOUND_EXCEPTION, cartDto.itemId(), cartDto.itemOptionId());
 		}
 	}
 
 	public List<CartResponse> findAll(final Long memberId) {
 		final List<CartDto> cartDtoList = cartRepository.findAllByMemberId(memberId);
-		final Set<Long> itemIdList = new HashSet<>();
-		final Set<Long> itemOptionIdList = new HashSet<>();
-		for (CartDto cartDto : cartDtoList) {
-			itemIdList.add(cartDto.itemId());
-			itemOptionIdList.add(cartDto.itemOptionId());
-		}
+		final List<ItemOption> itemOptionList = itemOptionRepository.findWithItemAndCategoryAndImageByItemIdListAndIdList(
+			cartDtoList);
 
-		final List<CartDto> sortedCartDtoList = cartDtoList.stream()
-			.sorted(Comparator.comparing(CartDto::itemId).thenComparing(CartDto::itemOptionId))
-			.toList();
-
-		final List<ItemOption> itemOptionList = itemOptionRepository.findWithItemAndCategory(itemIdList,
-			itemOptionIdList);
-
-		return CartResponseMapper.INSTANCE.from(itemOptionList, sortedCartDtoList);
+		return CartResponseMapper.INSTANCE.from(itemOptionList, cartDtoList);
 	}
 
 	@Transactional
