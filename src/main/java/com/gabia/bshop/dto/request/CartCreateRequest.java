815191055a680b175a36167c0ab36ba7a9e8a047
정종민 <jmchung@gabia.com>
2023-02-22 10:26:14 +0900

    refactor: 주문량에 0으로 들어오는 것을 valid 추가

--- src/main/java/com/gabia/bshop/dto/request/CartCreateRequest.java
+++ src/main/java/com/gabia/bshop/dto/request/CartCreateRequest.java
@@ -2,6 +2,7 @@ package com.gabia.bshop.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 
 public record CartCreateRequest(
 	@NotNull(message = "아이템 ID는 필수값입니다.")
@@ -11,7 +12,7 @@ public record CartCreateRequest(
 	Long itemOptionId,
 
 	@NotNull(message = "주문량은 필수값입니다.")
-	@Positive(message = "주문량은 양수만 허용합니다.")
+	@PositiveOrZero(message = "주문량은 양수만 허용합니다.")
 	int orderCount
 ) {
 }
