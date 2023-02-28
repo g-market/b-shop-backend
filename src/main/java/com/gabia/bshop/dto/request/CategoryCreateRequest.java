cd09d97071e806537f88410e773e8eded088b9c1
Jenna <gyjeon@gabia.com>
2023-02-28 15:38:14 +0900

    rename: dto 파일명 통일을 위한 변경

--- src/main/java/com/gabia/bshop/dto/request/CategoryCreateRequest.java
+++ src/main/java/com/gabia/bshop/dto/request/CategoryCreateRequest.java
@@ -4,7 +4,7 @@ import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record CategoryRequest(
+public record CategoryCreateRequest(
 	@Length(max = 255, message = "255자 이내로 입력해주세요.")
 	@NotBlank(message = "name은 필수 값입니다.")
 	String name
