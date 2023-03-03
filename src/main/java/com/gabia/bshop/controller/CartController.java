8598c51c7c38061a1840a28eae7368ccf5565972
정종민 <jmchung@gabia.com>
2023-03-03 17:45:27 +0900

    refactor: 다른 컨트롤러와 위치및 메서드 구성 통일화

--- src/main/java/com/gabia/bshop/controller/CartController.java
+++ src/main/java/com/gabia/bshop/controller/CartController.java
@@ -1,21 +1,20 @@
 package com.gabia.bshop.controller;
 
-import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabia.bshop.dto.CartDto;
 import com.gabia.bshop.dto.request.CartCreateRequest;
 import com.gabia.bshop.dto.request.CartDeleteRequest;
 import com.gabia.bshop.dto.response.CartResponse;
-import com.gabia.bshop.mapper.CartReqeuestMapper;
+import com.gabia.bshop.mapper.CartRequestMapper;
 import com.gabia.bshop.security.CurrentMember;
 import com.gabia.bshop.security.Login;
 import com.gabia.bshop.security.MemberPayload;
@@ -26,32 +25,30 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/carts")
 public class CartController {
 
 	private final CartService cartService;
 
-	@GetMapping
 	@Login
+	@GetMapping("/carts")
 	public ResponseEntity<List<CartResponse>> findCartList(@CurrentMember MemberPayload memberPayload) {
-		final List<CartResponse> responseList = cartService.findCartList(memberPayload.id());
-		return ResponseEntity.ok(responseList);
+		return ResponseEntity.ok(cartService.findCartList(memberPayload.id()));
 	}
 
-	@PostMapping
 	@Login
-	public ResponseEntity<Void> createCart(@CurrentMember MemberPayload memberPayload,
+	@PostMapping("/carts")
+	public ResponseEntity<CartDto> createCart(@CurrentMember MemberPayload memberPayload,
 		@RequestBody @Valid CartCreateRequest cartCreateRequest) {
-		final CartDto cartDto = CartReqeuestMapper.INSTANCE.toCartDto(cartCreateRequest);
-		cartService.createCart(memberPayload.id(), cartDto);
-		return ResponseEntity.created(URI.create("/carts")).build();
+		final CartDto cartDto = CartRequestMapper.INSTANCE.toCartDto(cartCreateRequest);
+		return ResponseEntity.status(HttpStatus.CREATED)
+			.body(cartService.createCart(memberPayload.id(), cartDto));
 	}
 
-	@DeleteMapping
 	@Login
+	@DeleteMapping("/carts")
 	public ResponseEntity<Void> deleteCart(@CurrentMember MemberPayload memberPayload,
 		@RequestBody @Valid CartDeleteRequest cartDeleteRequest) {
-		final CartDto cartDto = CartReqeuestMapper.INSTANCE.toCartDto(cartDeleteRequest);
+		final CartDto cartDto = CartRequestMapper.INSTANCE.toCartDto(cartDeleteRequest);
 		cartService.deleteCart(memberPayload.id(), cartDto);
 		return ResponseEntity.noContent().build();
 	}
