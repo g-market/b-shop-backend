e2a855b47546d828f88a3a7f6e3b52852ac1b4aa
백승주 <sjbaek@gabia.com>
2023-03-03 17:57:56 +0900

    feat : name, description 관련 Validation 로직 추가

--- src/main/java/com/gabia/bshop/dto/request/ItemRequest.java
+++ src/main/java/com/gabia/bshop/dto/request/ItemRequest.java
@@ -3,8 +3,11 @@ package com.gabia.bshop.dto.request;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.gabia.bshop.dto.ItemImageDto;
 import com.gabia.bshop.dto.ItemOptionDto;
+import com.gabia.bshop.dto.validator.ByteSize;
 import com.gabia.bshop.entity.enumtype.ItemStatus;
 
 import jakarta.validation.constraints.NotNull;
@@ -18,10 +21,12 @@ public record ItemRequest(
 	@NotNull(message = "categoryId 는 필수 입니다")
 	Long categoryId,
 
+	@Length(max = 255, message = "255자 이내로 입력해주세요.")
 	@NotNull(message = "name 을 입력하세요.")
 	String name,
 
 	@NotNull(message = "description 을 입력하세요")
+	@ByteSize(max = 64L, message = "description 의 크기가 허용된 크기 이상입니다.(64 Byte).")
 	String description,
 
 	@PositiveOrZero(message = "basePrice 는 0 이상입니다.")
