0eb07adecd620c97a081f63f10b9b79683f24e88
전가영 <gyjeon@gabia.com>
2023-03-06 23:55:00 +0900

    test: 카테고리 조회,수정,삭제 테스트 코드 추가

--- src/test/java/com/gabia/bshop/integration/service/CategoryServiceTest.java
+++ src/test/java/com/gabia/bshop/integration/service/CategoryServiceTest.java
@@ -2,28 +2,39 @@ package com.gabia.bshop.integration.service;
 
 import com.gabia.bshop.dto.CategoryDto;
 import com.gabia.bshop.dto.request.CategoryCreateRequest;
+import com.gabia.bshop.dto.request.CategoryUpdateRequest;
+import com.gabia.bshop.dto.response.ItemResponse;
 import com.gabia.bshop.entity.Category;
 import com.gabia.bshop.entity.Item;
 import com.gabia.bshop.entity.ItemOption;
 import com.gabia.bshop.fixture.CategoryFixture;
 import com.gabia.bshop.fixture.ItemFixture;
 import com.gabia.bshop.integration.IntegrationTest;
+import com.gabia.bshop.mapper.ItemMapper;
 import com.gabia.bshop.repository.CategoryRepository;
 import com.gabia.bshop.repository.ItemOptionRepository;
 import com.gabia.bshop.repository.ItemRepository;
 import com.gabia.bshop.service.CategoryService;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
 
 import static com.gabia.bshop.fixture.ItemOptionFixture.ITEM_OPTION_1;
 import static com.gabia.bshop.fixture.ItemOptionFixture.ITEM_OPTION_2;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @Transactional
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -33,37 +44,15 @@ class CategoryServiceTest extends IntegrationTest {
 	private CategoryRepository categoryRepository;
 
 	@Autowired
-	private ItemRepository itemRepository;
-
-	@Autowired
-	private ItemOptionRepository itemOptionRepository;
-
-	@Autowired
 	private CategoryService categoryService;
-
-	@Autowired
-	private EntityManager entityManager;
-
-	private final Long itemId = 1L;
-	private final Long itemOptionId = 1L;
-	private final Long newItemId = 2L;
-	private final Long newItemOptionId = 2L;
 
 	@DisplayName("카테고리를_생성한다.")
 	@Test
 	void createCategory() {
 		//given
 		final Category category1 = CategoryFixture.CATEGORY_1.getInstance();
-		final Item item1 = ItemFixture.ITEM_1.getInstance(itemId, category1);
-		final ItemOption itemOption1 = ITEM_OPTION_1.getInstance(itemOptionId, item1);
-		final Item item2 = ItemFixture.ITEM_2.getInstance(newItemId, category1);
-		final ItemOption itemOption2 = ITEM_OPTION_2.getInstance(newItemOptionId, item2);
 
 		categoryRepository.save(category1);
-		itemRepository.saveAll(List.of(item1, item2));
-		itemOptionRepository.saveAll(List.of(itemOption1, itemOption2));
-
-		//entityManager.clear();
 
 		//when
 		CategoryCreateRequest categoryCreateRequest = CategoryCreateRequest.builder()
@@ -73,5 +62,77 @@ class CategoryServiceTest extends IntegrationTest {
 		CategoryDto categoryDto = categoryService.createCategory(categoryCreateRequest);
 		//then
 		Assertions.assertThat(categoryDto.name()).isEqualTo(categoryCreateRequest.name());
+	}
+
+	@DisplayName("카테고리를_단건으로_조회한다.")
+	@Test
+	void findCategory() {
+		//given
+		final Category category1 = CategoryFixture.CATEGORY_1.getInstance();
+
+		categoryRepository.save(category1);
+
+		//when
+		Category category = categoryRepository.findById(category1.getId()).orElseThrow();
+		CategoryDto categoryDto = categoryService.findCategory(category.getId());
+
+		//then
+		Assertions.assertThat(categoryDto.name()).isEqualTo(category1.getName());
+		Assertions.assertThat(categoryDto.id()).isEqualTo(category1.getId());
+	}
+
+	@DisplayName("카테고리를_리스트로_조회한다.")
+	@Test
+	void findCategoryList() {
+		//given
+		final Category category1 = CategoryFixture.CATEGORY_1.getInstance();
+		final Category category2 = CategoryFixture.CATEGORY_2.getInstance();
+		final Category category3 = CategoryFixture.CATEGORY_3.getInstance();
+
+		categoryRepository.saveAll(List.of(category1,category2,category3));
+
+		//when
+		Pageable pageable = PageRequest.of(0, 10);
+		Page<CategoryDto> categoryDtoPage = categoryService.findCategoryList(pageable);
+
+		//then
+		List<CategoryDto> categoryDtoList = categoryDtoPage.getContent();
+		Assertions.assertThat(categoryDtoList.get(0).name()).isEqualTo("카테고리 1");
+		Assertions.assertThat(categoryDtoList.get(1).name()).isEqualTo("카테고리 2");
+		Assertions.assertThat(categoryDtoList.get(2).name()).isEqualTo("카테고리 3");
+	}
+
+	@DisplayName("카테고리를_수정한다.")
+	@Test
+	void updateCategory() {
+		//given
+		final Category category1 = CategoryFixture.CATEGORY_1.getInstance();
+
+		categoryRepository.save(category1);
+
+		//when
+		CategoryUpdateRequest categoryUpdateRequest = CategoryUpdateRequest.builder()
+				.id(category1.getId())
+				.name("카테고리 수정")
+				.build();
+		CategoryDto categoryDto = categoryService.updateCategory(categoryUpdateRequest);
+
+		//then
+		Assertions.assertThat(categoryDto.name()).isEqualTo(categoryUpdateRequest.name());
+		Assertions.assertThat(categoryDto.id()).isEqualTo(categoryUpdateRequest.id());
+	}
+
+	@DisplayName("카테고리를_삭제한다.")
+	@Test
+	void deleteCategory() {
+		//given
+		final Category category1 = CategoryFixture.CATEGORY_1.getInstance();
+		categoryRepository.save(category1);
+
+		//when
+		categoryService.deleteCategory(category1.getId());
+
+		//then
+		Assertions.assertThat(categoryRepository.findById(category1.getId()).isPresent()).isEqualTo(false);
 	}
 }
