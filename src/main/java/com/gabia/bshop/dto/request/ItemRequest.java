051bacfdb00a23aee93741e05b228c4f7dd1e093
백승주 <sjbaek@gabia.com>
2023-03-06 14:50:11 +0900

    refactor: ItemOption 개수 제한

--- src/main/java/com/gabia/bshop/dto/request/ItemRequest.java
+++ src/main/java/com/gabia/bshop/dto/request/ItemRequest.java
@@ -10,13 +10,19 @@ import com.gabia.bshop.dto.ItemOptionDto;
 import com.gabia.bshop.dto.validator.ByteSize;
 import com.gabia.bshop.entity.enumtype.ItemStatus;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record ItemRequest(
+	@Valid
+	@Size(max = 100, message = "ItemOption 이 허용된 수 이상입니다. (최대 100개)")
 	List<ItemOptionDto> itemOptionDtoList,
+
+	@Valid
 	List<ItemImageDto> itemImageDtoList,
 	@NotNull(message = "categoryId 는 필수 입니다")
 	Long categoryId,
@@ -30,12 +36,10 @@ public record ItemRequest(
 	String description,
 
 	@PositiveOrZero(message = "basePrice 는 0 이상입니다.")
-	@NotNull
 	int basePrice,
 	ItemStatus itemStatus,
 
 	@PositiveOrZero(message = "year 는 0 이상입니다.")
-	@NotNull
 	int year,
 
 	LocalDateTime openAt
