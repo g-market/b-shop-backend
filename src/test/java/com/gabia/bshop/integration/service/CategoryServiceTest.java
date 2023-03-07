e771fbd8b30ddbc759abeea3f717f96c6ef66afa
Jenna <gyjeon@gabia.com>
2023-03-07 09:06:06 +0900

    style: checkStyle 적용

--- src/test/java/com/gabia/bshop/integration/service/CategoryServiceTest.java
+++ src/test/java/com/gabia/bshop/integration/service/CategoryServiceTest.java
@@ -1,40 +1,25 @@
 package com.gabia.bshop.integration.service;
 
-import com.gabia.bshop.dto.CategoryDto;
-import com.gabia.bshop.dto.request.CategoryCreateRequest;
-import com.gabia.bshop.dto.request.CategoryUpdateRequest;
-import com.gabia.bshop.dto.response.ItemResponse;
-import com.gabia.bshop.entity.Category;
-import com.gabia.bshop.entity.Item;
-import com.gabia.bshop.entity.ItemOption;
-import com.gabia.bshop.fixture.CategoryFixture;
-import com.gabia.bshop.fixture.ItemFixture;
-import com.gabia.bshop.integration.IntegrationTest;
-import com.gabia.bshop.mapper.ItemMapper;
-import com.gabia.bshop.repository.CategoryRepository;
-import com.gabia.bshop.repository.ItemOptionRepository;
-import com.gabia.bshop.repository.ItemRepository;
-import com.gabia.bshop.service.CategoryService;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collections;
-import java.util.List;
-
-import static com.gabia.bshop.fixture.ItemOptionFixture.ITEM_OPTION_1;
-import static com.gabia.bshop.fixture.ItemOptionFixture.ITEM_OPTION_2;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import com.gabia.bshop.dto.CategoryDto;
+import com.gabia.bshop.dto.request.CategoryCreateRequest;
+import com.gabia.bshop.dto.request.CategoryUpdateRequest;
+import com.gabia.bshop.entity.Category;
+import com.gabia.bshop.fixture.CategoryFixture;
+import com.gabia.bshop.integration.IntegrationTest;
+import com.gabia.bshop.repository.CategoryRepository;
+import com.gabia.bshop.service.CategoryService;
 
 @Transactional
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -51,15 +36,14 @@ class CategoryServiceTest extends IntegrationTest {
 	void createCategory() {
 		//given
 		final Category category1 = CategoryFixture.CATEGORY_1.getInstance();
-
 		categoryRepository.save(category1);
 
 		//when
 		CategoryCreateRequest categoryCreateRequest = CategoryCreateRequest.builder()
-				.name("category test")
-				.build();
-
+			.name("category test")
+			.build();
 		CategoryDto categoryDto = categoryService.createCategory(categoryCreateRequest);
+
 		//then
 		Assertions.assertThat(categoryDto.name()).isEqualTo(categoryCreateRequest.name());
 	}
@@ -69,7 +53,6 @@ class CategoryServiceTest extends IntegrationTest {
 	void findCategory() {
 		//given
 		final Category category1 = CategoryFixture.CATEGORY_1.getInstance();
-
 		categoryRepository.save(category1);
 
 		//when
@@ -89,7 +72,7 @@ class CategoryServiceTest extends IntegrationTest {
 		final Category category2 = CategoryFixture.CATEGORY_2.getInstance();
 		final Category category3 = CategoryFixture.CATEGORY_3.getInstance();
 
-		categoryRepository.saveAll(List.of(category1,category2,category3));
+		categoryRepository.saveAll(List.of(category1, category2, category3));
 
 		//when
 		Pageable pageable = PageRequest.of(0, 10);
@@ -112,9 +95,9 @@ class CategoryServiceTest extends IntegrationTest {
 
 		//when
 		CategoryUpdateRequest categoryUpdateRequest = CategoryUpdateRequest.builder()
-				.id(category1.getId())
-				.name("카테고리 수정")
-				.build();
+			.id(category1.getId())
+			.name("카테고리 수정")
+			.build();
 		CategoryDto categoryDto = categoryService.updateCategory(categoryUpdateRequest);
 
 		//then
