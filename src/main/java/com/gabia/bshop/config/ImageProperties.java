2c09ca8c11181fdb643533a06aed6f0b9ac2df9f
jaime <jmchung@gabia.com>
2023-03-08 17:13:55 +0900

    feat: 환경변수로 Image Default URL 값을 매핑

---
+++ src/main/java/com/gabia/bshop/config/ImageProperties.java
@@ -0,0 +1,17 @@
+package com.gabia.bshop.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties("server.default")
+public class ImageProperties {
+
+	private String profileImageUrl;
+	private String itemImageUrl;
+}
