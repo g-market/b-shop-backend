d91994a7fff7ec263c195bc9f99077ecb8815958
jaime <jmchung@gabia.com>
2023-03-08 17:45:11 +0900

    test: item 연도를 distinct 쿼리를 통해 조회 하여 제공 메서드 테스트 및 테스트 코드 정리

--- src/test/java/com/gabia/bshop/service/ItemServiceTest.java
+++ src/test/java/com/gabia/bshop/service/ItemServiceTest.java
@@ -1,78 +1,60 @@
 package com.gabia.bshop.service;
 
+import static com.gabia.bshop.fixture.CategoryFixture.*;
+import static com.gabia.bshop.fixture.ItemFixture.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
-import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import com.gabia.bshop.config.ImageDefaultProperties;
 import com.gabia.bshop.dto.request.ItemUpdateRequest;
 import com.gabia.bshop.dto.response.ItemResponse;
 import com.gabia.bshop.entity.Category;
 import com.gabia.bshop.entity.Item;
-import com.gabia.bshop.entity.enumtype.ItemStatus;
 import com.gabia.bshop.exception.NotFoundException;
 import com.gabia.bshop.mapper.ItemMapper;
 import com.gabia.bshop.repository.CategoryRepository;
 import com.gabia.bshop.repository.ItemRepository;
 
-import jakarta.persistence.EntityNotFoundException;
-
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class ItemServiceTest {
 
-	List<Item> itemList;
 	@Mock
 	private CategoryRepository categoryRepository;
 	@Mock
 	private ItemRepository itemRepository;
+	@Mock
+	private ImageDefaultProperties imageDefaultProperties;
+
 	@InjectMocks
 	private ItemService itemService;
 
 	@Test
 	@DisplayName("상품을_조회한다")
 	void findItem() {
-		//given
-		Category category = Category.builder().id(1L).name("name").build();
-
-		Item item1 =
-			Item.builder()
-				.id(1L)
-				.category(category)
-				.name("item")
-				.itemStatus(ItemStatus.PUBLIC)
-				.basePrice(10000)
-				.description("description")
-				.openAt(LocalDateTime.now())
-				.build();
-
-		Item item2 =
-			Item.builder()
-				.id(2L)
-				.category(category)
-				.name("item2")
-				.itemStatus(ItemStatus.PUBLIC)
-				.basePrice(10000)
-				.description("description")
-				.openAt(LocalDateTime.now())
-				.build();
+		// given
+		Category category = CATEGORY_1.getInstance(1L);
+		Item item1 = ITEM_1.getInstance(1L, category);
+		Item item2 = ITEM_2.getInstance(2L, category);
 
 		// when
-		when(categoryRepository.findById(1L)).thenReturn(Optional.ofNullable(category));
 		when(itemRepository.findById(1L)).thenReturn(Optional.ofNullable(item1));
 		when(itemRepository.findById(2L)).thenReturn(Optional.ofNullable(item2));
 
@@ -84,38 +66,16 @@ class ItemServiceTest {
 	@Test
 	@DisplayName("상품_목록을_조회한다")
 	void findItemList() {
-		// givn
-		Category category = Category.builder().id(1L).name("name").build();
-
-		Item item1 =
-			Item.builder()
-				.id(1L)
-				.category(category)
-				.name("item")
-				.itemStatus(ItemStatus.PUBLIC)
-				.basePrice(10000)
-				.description("description")
-				.openAt(LocalDateTime.now())
-				.build();
-
-		Item item2 =
-			Item.builder()
-				.id(2L)
-				.category(category)
-				.name("item2")
-				.itemStatus(ItemStatus.PUBLIC)
-				.basePrice(10000)
-				.description("description")
-				.openAt(LocalDateTime.now())
-				.build();
-
-		itemList = List.of(item1, item2);
+		// given
+		Category category = CATEGORY_1.getInstance(1L);
+		Item item1 = ITEM_1.getInstance(1L, category);
+		Item item2 = ITEM_2.getInstance(2L, category);
 
 		Pageable pageable = PageRequest.of(0, 10);
 		Page<ItemResponse> itemDtoList = new PageImpl<>(
-			itemList.stream().map(ItemMapper.INSTANCE::itemToItemResponse).toList());
+			Stream.of(item1, item2).map(ItemMapper.INSTANCE::itemToItemResponse).toList());
 
-		Page<Item> itemPage = new PageImpl<>(Collections.unmodifiableList(itemList));
+		Page<Item> itemPage = new PageImpl<>(List.of(item1, item2));
 
 		when(itemRepository.findAll(pageable)).thenReturn(itemPage);
 
@@ -127,109 +87,62 @@ class ItemServiceTest {
 	@DisplayName("상품을_수정한다")
 	void changeItem() {
 		// given
-		Category category = Category.builder().id(1L).name("name").build();
+		Category category = CATEGORY_1.getInstance(1L);
 
-		Item item1 =
-			Item.builder()
-				.id(2L)
-				.category(category)
-				.name("item2")
-				.itemStatus(ItemStatus.PUBLIC)
-				.basePrice(10000)
-				.description("before")
-				.openAt(LocalDateTime.now())
-				.build();
-
-		Item item2 =
-			Item.builder()
-				.id(2L)
-				.category(category)
-				.name("item2")
-				.itemStatus(ItemStatus.PUBLIC)
-				.basePrice(20000)
-				.description("changed")
-				.openAt(LocalDateTime.now())
-				.build();
-
-		ItemUpdateRequest itemDto = ItemMapper.INSTANCE.itemToItemChangeRequest(item2);
+		Item item1 = ITEM_1.getInstance(1L, category);
+		Item item2 = ITEM_2.getInstance(1L, category);
+		ItemUpdateRequest itemDto = ItemMapper.INSTANCE.itemToItemUpdateRequest(item2);
 
 		// when
+		when(itemRepository.findById(1L)).thenReturn(Optional.ofNullable(item1));
 		when(categoryRepository.findById(1L)).thenReturn(Optional.ofNullable(category));
-		when(itemRepository.findById(2L)).thenReturn(Optional.ofNullable(item1));
-		when(itemRepository.save(item2)).thenReturn(item2);
 
 		// then
-		ItemResponse changedItem = itemService.updateItem(itemDto);
+		ItemResponse itemResponse = itemService.updateItem(itemDto);
 
 		assertAll(
-			() -> assertEquals(20000, changedItem.basePrice()),
-			() -> assertEquals("changed", changedItem.description()));
+			() -> assertEquals(20000, itemResponse.basePrice()),
+			() -> assertEquals(itemResponse.description(), item2.getDescription()));
 	}
 
 	@Test
 	@DisplayName("상품을_생성한다")
 	void createItem() {
 		// given
-		Category category = Category.builder().id(1L).name("name").build();
+		Category category = CATEGORY_1.getInstance(1L);
 
-		Item item =
-			Item.builder()
-				.id(1L)
-				.category(category)
-				.name("item")
-				.itemStatus(ItemStatus.PUBLIC)
-				.basePrice(20000)
-				.description("item1")
-				.openAt(LocalDateTime.now())
-				.build();
-
-		Item returnItem = Item.builder()
-			.id(1L)
-			.category(category)
-			.name("item")
-			.itemStatus(ItemStatus.PUBLIC)
-			.basePrice(20000)
-			.description("item1")
-			.openAt(LocalDateTime.now())
-			.build();
+		Item item = ITEM_1.getInstance(category);
+		Item returnItem = ITEM_1.getInstance(1L, category);
 
 		// when
-		when(categoryRepository.findById(1L)).thenReturn(Optional.ofNullable(category));
+		when(categoryRepository.findById(1L)).thenReturn(Optional.of(category));
+		when(imageDefaultProperties.getItemImageUrl()).thenReturn("http://localhost:9000/images/No_Image.jpg");
 		when(itemRepository.save(any())).thenReturn(returnItem);
 
-		ItemResponse itemDto = itemService.createItem(ItemMapper.INSTANCE.itemToItemRequest(item));
+		ItemResponse itemResponse = itemService.createItem(ItemMapper.INSTANCE.itemToItemCreateRequest(item));
 
 		// then
 		assertAll(
-			() -> assertEquals(itemDto.categoryDto().id(), item.getCategory().getId()),
-			() -> assertEquals(itemDto.name(), item.getName()),
-			() -> assertEquals(itemDto.basePrice(), item.getBasePrice()),
-			() -> assertEquals(itemDto.description(), item.getDescription())
+			() -> assertEquals(itemResponse.categoryDto().id(), item.getCategory().getId()),
+			() -> assertEquals(itemResponse.name(), item.getName()),
+			() -> assertEquals(itemResponse.basePrice(), item.getBasePrice()),
+			() -> assertEquals(itemResponse.description(), item.getDescription())
 		);
 
 	}
 
 	@Test
 	@DisplayName("상품_수정시_상품이_없으면_실패한다")
-	void failToDeleteItemWithNoitem() {
+	void failToDeleteItemWithNoItem() {
 		// given
-		Category category = Category.builder().id(1L).name("name").build(); // 존재하지 않는 카테고리
+		Category category = CATEGORY_1.getInstance(1L); // 존재하지 않는 카테고리
 
-		Item item =
-			Item.builder()
-				.id(3L)
-				.category(category)
-				.name("item3")
-				.itemStatus(ItemStatus.PUBLIC)
-				.basePrice(20000)
-				.description("not exist")
-				.openAt(LocalDateTime.now())
-				.build();
+		Item item = ITEM_3.getInstance(3L, category);
 
-		ItemUpdateRequest itemUpdateRequest = ItemMapper.INSTANCE.itemToItemChangeRequest(item);
+		ItemUpdateRequest itemUpdateRequest = ItemMapper.INSTANCE.itemToItemUpdateRequest(item);
 
 		// when
-		when(itemRepository.save(item)).thenReturn(item);
+		when(itemRepository.findById(item.getId())).thenReturn(Optional.empty());
 
 		// then
 		Assertions.assertThrows(
@@ -243,23 +156,13 @@ class ItemServiceTest {
 	@DisplayName("상품_수정시_카테고리가_없으면_실패한다")
 	void failToChangeItemWithoutCategory() {
 		// given
-		Category category = Category.builder().id(2L).name("name").build(); // 존재하지 않는 카테고리
-
-		Item item =
-			Item.builder()
-				.id(2L)
-				.category(category)
-				.name("item2")
-				.itemStatus(ItemStatus.PUBLIC)
-				.basePrice(20000)
-				.description("changed")
-				.openAt(LocalDateTime.now())
-				.build();
-
-		ItemUpdateRequest itemUpdateRequest = ItemMapper.INSTANCE.itemToItemChangeRequest(item);
+		Category category = CATEGORY_1.getInstance(1L); // 존재하지 않는 카테고리
+		Item item = ITEM_2.getInstance(1L, category);
+		ItemUpdateRequest itemUpdateRequest = ItemMapper.INSTANCE.itemToItemUpdateRequest(item);
 
 		// when
-		when(categoryRepository.findById(2L)).thenThrow(EntityNotFoundException.class);
+		when(itemRepository.findById(1L)).thenReturn(Optional.of(item));
+		when(categoryRepository.findById(1L)).thenThrow(NotFoundException.class);
 
 		// then
 		Assertions.assertThrows(
@@ -274,18 +177,9 @@ class ItemServiceTest {
 	void itemDeleteSuccess() {
 		// given
 		Long itemId1 = 1L;
-		Category category = Category.builder().id(1L).name("name").build(); // 존재하지 않는 카테고리
+		Category category = CATEGORY_1.getInstance(1L); // 존재하지 않는 카테고리
 
-		Item item1 =
-			Item.builder()
-				.id(itemId1)
-				.category(category)
-				.name("item2")
-				.itemStatus(ItemStatus.PUBLIC)
-				.basePrice(20000)
-				.description("delete test item")
-				.openAt(LocalDateTime.now())
-				.build();
+		Item item1 = ITEM_1.getInstance(1L, category);
 		// when
 		when(itemRepository.findById(itemId1)).thenReturn(Optional.ofNullable(item1));
 		itemService.deleteItem(itemId1);
@@ -309,5 +203,22 @@ class ItemServiceTest {
 			() -> {
 				itemService.deleteItem(itemId);
 			});
+	}
+
+	@Test
+	@DisplayName("상품 연도를 조회하면 상품에서 존재하는 연도를 다 보여준다")
+	void findItemYears() {
+		// given
+		List<Integer> itemYearList = List.of(2023, 2022, 2021, 2020, 2019);
+		when(itemRepository.findItemYears()).thenReturn(itemYearList);
+
+		// when
+		List<Integer> result = itemService.findItemYears();
+
+		// then
+		assertAll(
+			() -> verify(itemRepository).findItemYears(),
+			() -> assertThat(result).usingRecursiveComparison().isEqualTo(itemYearList)
+		);
 	}
 }
