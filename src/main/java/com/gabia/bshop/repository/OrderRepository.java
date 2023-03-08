d67a078e728d4f081fdb6bf349d2c76cf752e078
jaime <jmchung@gabia.com>
2023-03-09 00:03:32 +0900

    remove: 사용하지 않는 메서드들 삭제

--- src/main/java/com/gabia/bshop/repository/OrderRepository.java
+++ src/main/java/com/gabia/bshop/repository/OrderRepository.java
@@ -1,10 +1,8 @@
 package com.gabia.bshop.repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
@@ -13,24 +11,7 @@ import com.gabia.bshop.entity.Order;
 
 import jakarta.persistence.LockModeType;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-	// TODO: created_at 인덱스 생성
-	@Query("""
-		select o
-		from Order o
-		where o.member.id =:memberId
-		order by o.createdAt
-		""")
-	List<Order> findByMemberIdPagination(Long memberId, Pageable pageable);
-
-	@Query("""
-		select o
-		from Order o
-		where o.createdAt >= :startAt and o.createdAt <= :endAt
-		order by o.createdAt
-		""")
-	List<Order> findAllByPeriodPagination(LocalDateTime startAt, LocalDateTime endAt, Pageable pageable);
+public interface OrderRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
 
 	Optional<Order> findByIdAndMemberId(Long orderId, Long memberId);
 
