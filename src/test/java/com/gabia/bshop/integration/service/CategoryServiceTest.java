2f3419803c6a85e1465b5440b3e89b8eaf219583
jaime <jmchung@gabia.com>
2023-03-15 00:20:40 +0900

    test: Category Test 코드 추가

--- src/test/java/com/gabia/bshop/integration/service/CategoryServiceTest.java
+++ src/test/java/com/gabia/bshop/integration/service/CategoryServiceTest.java
@@ -1,5 +1,7 @@
 package com.gabia.bshop.integration.service;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
@@ -15,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.gabia.bshop.dto.CategoryDto;
 import com.gabia.bshop.dto.request.CategoryCreateRequest;
 import com.gabia.bshop.dto.request.CategoryUpdateRequest;
+import com.gabia.bshop.dto.response.CategoryAllInfoResponse;
 import com.gabia.bshop.entity.Category;
 import com.gabia.bshop.fixture.CategoryFixture;
 import com.gabia.bshop.repository.CategoryRepository;
@@ -45,6 +48,22 @@ class CategoryServiceTest {
 		Assertions.assertThat(categoryDto.name()).isEqualTo(categoryCreateRequest.name());
 	}
 
+	@DisplayName("카테고리를 생성할 때 존재하는 이름이면 예외를 던진다.")
+	@Test
+	void createDuplicatedCategory() {
+		// given
+		final Category category1 = CategoryFixture.CATEGORY_1.getInstance();
+		categoryRepository.save(category1);
+
+		// when
+		CategoryCreateRequest categoryCreateRequest = new CategoryCreateRequest(category1.getName());
+
+		//when & then
+		Assertions.assertThatThrownBy(
+			() -> categoryService.createCategory(categoryCreateRequest)
+		);
+	}
+
 	@DisplayName("카테고리를_단건으로_조회한다.")
 	@Test
 	void findCategory() {
@@ -59,6 +78,71 @@ class CategoryServiceTest {
 		//then
 		Assertions.assertThat(categoryDto.name()).isEqualTo(category1.getName());
 		Assertions.assertThat(categoryDto.id()).isEqualTo(category1.getId());
+	}
+
+	@DisplayName("일반 사용자는 삭제된 카테고리를 단건으로 조회 하지 못한다.")
+	@Test
+	void findCategoryWhenNormalUser() {
+		// given
+		final Category category1 = CategoryFixture.CATEGORY_1.getInstance();
+		categoryRepository.save(category1);
+		categoryRepository.delete(category1);
+
+		// when & then
+		Assertions.assertThatThrownBy(
+			() -> categoryService.findCategory(category1.getId())
+		);
+	}
+
+	@DisplayName("관리자는 삭제된 카테고리를 단건으로 조회한다.")
+	@Test
+	void findCategoryWhenAdmin() {
+		// given
+		final Category category1 = CategoryFixture.CATEGORY_1.getInstance();
+		categoryRepository.save(category1);
+		categoryRepository.delete(category1);
+		categoryRepository.flush();
+
+		// when
+		CategoryAllInfoResponse response = categoryService.findCategoryWithDeleted(category1.getId());
+
+		// then
+		assertAll(
+			() -> Assertions.assertThat(response.id()).isEqualTo(category1.getId()),
+			() -> Assertions.assertThat(response.name()).isEqualTo(category1.getName()),
+			() -> Assertions.assertThat(response.deleted()).isEqualTo(true)
+		);
+
+	}
+
+	@DisplayName("관리자는 삭제된 카테고리를 리스트로 조회한다.")
+	@Test
+	void findCategoryListWithDeleted() {
+		// given
+		final Category category1 = CategoryFixture.CATEGORY_1.getInstance();
+		final Category category2 = CategoryFixture.CATEGORY_2.getInstance();
+		final Category category3 = CategoryFixture.CATEGORY_3.getInstance();
+		categoryRepository.saveAll(List.of(category1, category2, category3));
+		categoryRepository.deleteAll(List.of(category1, category2, category3));
+		categoryRepository.flush();
+
+		final CategoryAllInfoResponse categoryAllInfoResponse1 = new CategoryAllInfoResponse(category1.getId(),
+			category1.getName(), true);
+		final CategoryAllInfoResponse categoryAllInfoResponse2 = new CategoryAllInfoResponse(category2.getId(),
+			category2.getName(), true);
+		final CategoryAllInfoResponse categoryAllInfoResponse3 = new CategoryAllInfoResponse(category3.getId(),
+			category3.getName(), true);
+
+		// when
+		Pageable pageable = PageRequest.ofSize(12);
+		Page<CategoryAllInfoResponse> response = categoryService.findCategoryListWithDeleted(pageable);
+		List<CategoryAllInfoResponse> content = response.getContent();
+
+		// then
+		assertAll(
+			() -> Assertions.assertThat(content).usingRecursiveComparison().isEqualTo(List.of(categoryAllInfoResponse1,
+				categoryAllInfoResponse2, categoryAllInfoResponse3))
+		);
 	}
 
 	@DisplayName("카테고리를_페이지로_조회한다.")
@@ -126,7 +210,7 @@ class CategoryServiceTest {
 		final List<String> categoryNames = categoryService.findCategoryNames();
 
 		// then
-		Assertions.assertThat(categoryRepository.findCategoryNames())
+		Assertions.assertThat(categoryNames)
 			.usingRecursiveComparison()
 			.isEqualTo(List.of(category1.getName(), category2.getName(), category3.getName(), category4.getName(),
 				category5.getName()));
