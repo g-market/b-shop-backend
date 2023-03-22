15e2172beb637f230e236cf21dfd88837a59bdaa
Jenna <gyjeon@gabia.com>
2023-03-22 17:27:12 +0900

    refactor: 불필요한 주석 제거 및 checkstyle 적용

--- src/main/java/com/gabia/bshop/repository/OrderRepository.java
+++ src/main/java/com/gabia/bshop/repository/OrderRepository.java
@@ -4,12 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
 
 import com.gabia.bshop.entity.Order;
-
-import jakarta.persistence.LockModeType;
 
 public interface OrderRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
 
--- src/main/java/com/gabia/bshop/service/OrderFacadeService.java
+++ src/main/java/com/gabia/bshop/service/OrderFacadeService.java
@@ -1,6 +1,5 @@
 package com.gabia.bshop.service;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
