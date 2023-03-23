f80a1425a68e2755e80d469e699f204376cca07e
전가영 <gyjeon@gabia.com>
2023-03-23 11:43:24 +0900

    feat: Redisson을 이용한 재고 관련 Lock 구현 (mentee/mentee_2023.01/team/g-market/gabia_b_shop_backend!72)
    * chore: redisson implementation group -> implementation으로 변경
    
    * test: test coverage를 위한 예외 test code 추가
    
    * refactor: 불필요한 주석 제거 및 checkstyle 적용
    
    * refactor: 사용하지 않는 메서드 제거
    
    * refactor: 불필요한 코드 제거
    
    * feat: redisson config 설정 및 lock 적용

--- src/main/java/com/gabia/bshop/repository/OrderRepository.java
+++ src/main/java/com/gabia/bshop/repository/OrderRepository.java
@@ -4,25 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
 
 import com.gabia.bshop.entity.Order;
-
-import jakarta.persistence.LockModeType;
 
 public interface OrderRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
 
 	Optional<Order> findByIdAndMemberId(Long orderId, Long memberId);
-
-	@Query("""
-		select o from Order o
-		join fetch o.orderItemList
-		where o.id = :orderId
-		and o.member.id = :memberId
-		""")
-	@Lock(LockModeType.PESSIMISTIC_WRITE)
-	Optional<Order> findByIdAndMemberIdWithLock(Long orderId, Long memberId);
 
 	List<Order> findAllByMemberId(Long memberId);
 }
